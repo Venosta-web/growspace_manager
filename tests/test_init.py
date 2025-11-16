dd32253e8829f6e74de6f8d6abceacb31c5f977f
Venosta-web <popeepopepope@gmail.com>
2025-11-16 17:59:22 +0000

    proper implementation off recorder_mock

--- tests/test_init.py
+++ tests/test_init.py
@@ -1,20 +1,19 @@
 """Test the Growspace Manager integration."""
 
 import pytest
-from unittest.mock import AsyncMock, MagicMock
 
 from homeassistant.core import HomeAssistant
+from homeassistant.setup import async_setup_component
+
 from pytest_homeassistant_custom_component.common import MockConfigEntry
+from custom_components.growspace_manager import async_unload_entry
 from custom_components.growspace_manager.const import DOMAIN
 
 
 @pytest.mark.asyncio
-async def test_async_setup_entry(
-    hass: HomeAssistant, enable_custom_integrations, recorder_mock
-):
+async def test_async_setup_entry(recorder_mock: HomeAssistant):
     """Test a successful setup entry."""
-    from homeassistant.setup import async_setup_component
-
+    hass = recorder_mock  # Use the hass object provided by recorder_mock
     entry = MockConfigEntry(domain=DOMAIN, data={}, options={})
     entry.add_to_hass(hass)
     assert await async_setup_component(hass, DOMAIN, {})
@@ -22,14 +21,10 @@ async def test_async_setup_entry(
 
 
 @pytest.mark.asyncio
-async def test_async_unload_entry(
-    hass: HomeAssistant, enable_custom_integrations, recorder_mock
-):
+async def test_async_unload_entry(recorder_mock: HomeAssistant):
     """Test a successful unload entry."""
-    from custom_components.growspace_manager import async_unload_entry
-    from homeassistant.setup import async_setup_component
-
-    entry = MockConfigEntry(domain=DOMAIN, data={}, options={}, entry_id="test")
+    hass = recorder_mock  # Use the hass object provided by recorder_mock
+    entry = MockConfigEntry(domain=DOMAIN, data={}, options={})
     entry.add_to_hass(hass)
     assert await async_setup_component(hass, DOMAIN, {})
     await hass.async_block_till_done()
