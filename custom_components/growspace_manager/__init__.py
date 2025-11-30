b683f40969e47a89b920fff99f88236f3951947b
Venosta-web <popeepopepope@gmail.com>
2025-11-30 18:30:58 +0100

    fixed dehumidifier coordinator isn't async

--- custom_components/growspace_manager/__init__.py
+++ custom_components/growspace_manager/__init__.py
@@ -139,7 +139,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         ] = irrigation_coordinator
 
         dehumidifier_coordinator = DehumidifierCoordinator(hass, entry, growspace_id, coordinator)
-        await dehumidifier_coordinator.async_setup()
+        
         hass.data[DOMAIN][entry.entry_id]["dehumidifier_coordinators"][
             growspace_id
         ] = dehumidifier_coordinator
